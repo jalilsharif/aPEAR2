f714bd9b71e6726f0049b430ee03ec86665e9008
kerberus <jalilahmad.sharif@gmail.com>
2024-08-30 14:59:07 +0100

    added export function and added new file

---
+++ R/findSimilarity.R
@@ -0,0 +1,64 @@
+
+#' 
+#' Find Similarity Coefficients
+#' 
+#' @description Calculates the similarities only within the enrichment data based on
+#' pathway similarity.
+#' 
+#' @param enrichment a data.frame containing enrichment results
+#' @param methods methods for calculating the pathway clusters within the enrichment
+#' result (object of class aPEAR.methods; default: aPEAR.methods)
+#' @param verbose enable / disable log messages (default: FALSE)
+#' @param ... additional parameters (see \code{?aPEAR.methods})
+#'
+#' @return a list of two objects: \code{sim} - pathway similarity matrix; and
+#' \code{clusters} - pathway clusters
+#'
+#' @importFrom methods is
+#' @importFrom data.table .I
+#'
+#' @examples
+#' \donttest{
+#' # Load libraries
+#' library(clusterProfiler)
+#' library(DOSE)
+#' library(org.Hs.eg.db)
+#' data(geneList)
+#'
+#' # Perform enrichment using clusterProfiler
+#' enrich <- gseGO(geneList, OrgDb = org.Hs.eg.db, ont = 'CC')
+#'
+#' # Obtain clusters within the enriched pathways using default parameters
+#' data <- findSimilarity(enrich@result)
+#' data$clusters
+#'
+#' # Obtain clusters within the enriched pathways using hierarchical clustering
+#' # and minClusterSize = 1
+#' data <- findSimilarity(enrich@result, cluster = 'hier', minClusterSize = 1)
+#' data$clusters
+#' }
+#' 
+#' @export
+#'
+#' @return a list of similarity matrix
+#' 
+findSimilarity <- function(
+  enrichment,
+  methods = aPEAR.methods,
+  verbose = FALSE,
+  ...
+) {
+  if (!methods::is(enrichment, 'data.frame')) {
+    stop(paste0('Unrecognized data type for parameter "enrichment": ',
+                paste(class(enrichment), collapse = ', '), '. Please provide a data.frame.'))
+  }
+
+  if (verbose) message('Validating parameters...')
+  methods <- prepareMethods(methods, ...)
+
+  if (verbose) message('Validating enrichment data...')
+  data <- prepareEnrichment(enrichment, methods = methods, verbose = verbose, requireTheme = FALSE)
+
+  sim <- similarity(values = data$genes, method = methods$similarity, verbose = verbose)
+  return(similarity = sim)
+}
